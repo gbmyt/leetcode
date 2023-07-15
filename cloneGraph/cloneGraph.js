4a7d4fb2fdd485f34bc8bbf9441621589ac297fb
gbmytabc1@gmail.com <gbmytabc1@gmail.com>
2023-07-15 15:28:20 -0400

    Leetcode Med | cloneGraph Added Neetcode JS Solution

--- cloneGraph/cloneGraph.js
+++ cloneGraph/cloneGraph.js
@@ -37,6 +37,29 @@ function Node(val, neighbors) {
 //           	    NeetCode Solution
 // =============================================
 // Time Complexity: O(n)
+var cloneGraph = function(node, seen = new Map()) {
+  const isBaseCase = node === null;
+  if (isBaseCase) return null;
+
+  if (seen.has(node)) return seen.get(node);
+
+  return dfs(node, seen);                              /* Time O(V + E) | Space O(N) */
+}
+
+const dfs = (node, seen) => {
+  const clone = new Node(node.val);
+
+  seen.set(node, clone);                               /*               | Space O(N) */
+
+  for (const neighbor of node.neighbors) {
+      const cloneNeighbor = cloneGraph(neighbor, seen);/* Time O(V + E) | Space O(H) */
+
+      clone.neighbors.push(cloneNeighbor);             /*               | Space O(V + E) */
+  }
+
+  return clone;
+}
+
 
 // =============================================
 //           	  My Solution (Broken)
